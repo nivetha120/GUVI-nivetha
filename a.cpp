c0c95bd289083689e4ddf8008a783dccb2195709
nivetha120 <shanmugamnivetha6@gmail.com>
2016-06-06 20:45:12 +0530

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int a;
+ cin>>a;
+ if(a>1)
+ cout<<"positive";
+ if(a<1&&a!=0)
+ cout<<"negative";
+ if(a==0)
+ cout<<"zero";
+ return 0;
+ }
